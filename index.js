248f423ba9b7dbabd45e0b18df12f43ad074f419
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-12 21:26:01 +0300

    finalized

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//changing div color
+
+const mydiv=document.querySelector(".divfill")
+
+
 
 //getting all classes with btn
 const mycolors=document.querySelectorAll(".btn")
@@ -8,7 +13,32 @@ mycolors.forEach((mybtn)=>{
     mybtn.addEventListener('click',(e)=>{
         let colorchange=e.currentTarget.classList
         if(colorchange.contains('black')){
-            
+            mydiv.style.backgroundColor="black"
+            mydiv.textContent='black'
+        }
+        else if(colorchange.contains('blue')){
+            mydiv.style.backgroundColor="blue"
+            mydiv.textContent='blue'
+        }
+        else if(colorchange.contains('red')){
+            mydiv.style.backgroundColor="red"
+            mydiv.textContent='Red'
+        }
+        else if(colorchange.contains('yellow')){
+            mydiv.style.backgroundColor="yellow"
+            mydiv.textContent='yellow'
+        }
+        else if(colorchange.contains('green')){
+            mydiv.style.backgroundColor="green"
+            mydiv.textContent='green'
+        }
+        else if(colorchange.contains('brown')){
+            mydiv.style.backgroundColor="brown"
+            mydiv.textContent='brown'
+        }
+        else{
+            mydiv.style.backgroundColor="pink"
+            mydiv.textContent='no color picked'
         }
     })
 })
